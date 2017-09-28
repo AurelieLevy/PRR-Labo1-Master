5e6dd5fc5fd82ee0b162bc099e6f8fe21e84d878
Nadir Benallal <nadir.benallal@heig-vd.ch>
2017-09-28 10:52:25 +0200

    initial commit. Creation of the master project

---
+++ SynchroTime/src/synchrotime/SynchroTime.java
@@ -0,0 +1,14 @@
+package synchrotime;
+
+public class SynchroTime
+{
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args)
+   {
+      
+   }
+   
+}
