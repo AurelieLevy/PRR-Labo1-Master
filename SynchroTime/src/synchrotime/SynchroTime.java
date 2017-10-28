668f14dd4a02d6d982ffa90c4ed26e242b4cc094
Aurelie Levy <aurelie.levy@heig-vd.ch>
2017-10-28 16:26:12 +0200

    petite modif

--- SynchroTime/src/synchrotime/SynchroTime.java
+++ SynchroTime/src/synchrotime/SynchroTime.java
@@ -4,27 +4,23 @@ import java.net.SocketException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class SynchroTime
-{
+public class SynchroTime {
 
    /**
     * @param args the command line arguments
     */
-   public static void main(String[] args)
-   {      
-      try
-      {
-        Thread multicast = new Thread(new MulticastManager("239.10.10.1", 2223));
-        Thread communication = new Thread(new MessageManager(2222));
-         
-        multicast.start();
-        communication.start();
-         
-      } catch (SocketException ex)
-      {
+   public static void main(String[] args) {
+      try {
+         Thread multicast = new Thread(new MulticastManager("239.10.10.1", 2223));
+         Thread communication = new Thread(new MessageManager(2222));
+
+         multicast.start();
+         communication.start();
+
+      } catch (SocketException ex) {
          Logger.getLogger(SynchroTime.class.getName()).log(Level.SEVERE, null, ex);
          System.out.println("Thread not created");
       }
    }
-   
+
 }
